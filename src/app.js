4a2c39be55348a16eb78c7a19d984a94c13ddd0f
Celso Bufano <cbufano@kapatron.com.br>
2020-05-11 11:14:31 -0300

    Skills CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid , isUuid} = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,54 @@ app.use(cors());
 
 const repositories = [];
 
+//Listar
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
+//Adicionar
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title , url, techs } = request.body;
+  const likes = 0;
+  const dados = {id:uuid(),url,title,techs,likes};
+  repositories.push(dados);
+  return response.json(dados);
 });
 
+//Alterar
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title , url, techs } = request.body;
+  const repositoriIndex = repositories.findIndex(repositories => repositories.id === id );
+  if(repositoriIndex < 0) {
+    return response.status(400).json({error: 'put - title not found.'});
+  } 
+  const likes = repositories[repositoriIndex].likes;
+  const repositori = {id , title , url, techs ,likes} ;
+  repositories[repositoriIndex] = repositori;
+  return response.json(repositori); 
 });
 
+//Delete
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositoriIndex = repositories.findIndex(repository => repository.id === id );
+  if(repositoriIndex < 0) {
+    return response.status(400).json({error: 'title not found.'});
+  } 
+  repositories.splice(repositoriIndex,1);
+  return response.status(204).send();
 });
 
+//Likes
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  var repository = repositories.find(repository => repository.id === id );
+  if(!repository) {
+    return response.status(400).send();
+  } 
+  repository.likes += parseInt("1") ;
+  return response.json(repository); 
 });
 
 module.exports = app;
